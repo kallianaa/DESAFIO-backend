c42a080fd8c14a8036c54c15dbfbb72f005eaedd
Dayane Rodrigues <dayanealrodrigues@gmail.com>
2025-11-15 10:23:00 -0300

    consertando conflitos

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,17 +1,5 @@
 const express = require('express');
-<<<<<<< HEAD
-const { registerUser, loginUser } = require('../controllers/auth.controller');
 
-const router = express.Router();
-
-// POST /api/auth/register
-router.post('/register', registerUser);
-
-router.post('/login', loginUser);
-
-module.exports = router;
-// POST /api/
-=======
 const router = express.Router();
 const AuthController = require('../controllers/AuthController');
 // Rota para login
@@ -19,4 +7,3 @@ router.post('/login', AuthController.login);
 router.post('/refresh', AuthController.refresh);
 
 module.exports = router;
->>>>>>> main
